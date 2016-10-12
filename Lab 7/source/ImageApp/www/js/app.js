6bf8f2b79748496a82224463fb95c3decc6eebdd
liuyunl777 <yunlongliu@yunlongs-mbp.kc.umkc.edu>
2016-10-12 18:10:12 -0500

    lab 7 update

---
+++ Lab 7/source/ImageApp/www/js/app.js
@@ -0,0 +1,142 @@
+var imageApp=angular.module("starter", ["ionic","ngCordova","firebase"]);
+
+imageApp.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+imageApp.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state("firebase", {
+            url: "/firebase",
+            templateUrl: "templates/firebase.html",
+            controller: "FirebaseController",
+            cache: false
+        })
+        .state("secure", {
+            url: "/secure",
+            templateUrl: "templates/secure.html",
+            controller: "SecureController"
+        });
+    $urlRouterProvider.otherwise('/firebase');
+});
+
+imageApp.controller("FirebaseController", function($scope, $state, $firebaseAuth) {
+
+    var fbAuth = $firebaseAuth();
+
+    $scope.login = function(username, password) {
+        fbAuth.$signInWithEmailAndPassword(username,password).then(function(authData) {
+            $state.go("secure");
+        }).catch(function(error) {
+            console.error("ERROR: " + error);
+        });
+    }
+
+    $scope.register = function(username, password) {
+        fbAuth.$createUserWithEmailAndPassword(username,password).then(function(userData) {
+            return fbAuth.$signInWithEmailAndPassword(username,
+                password);
+        }).then(function(authData) {
+            $state.go("secure");
+        }).catch(function(error) {
+            console.error("ERROR: " + error);
+        });
+    }
+
+});
+
+//secure controller
+
+imageApp.controller("SecureController", function($scope, $ionicHistory, $ionicActionSheet, $firebaseObject, $firebaseArray, $firebaseAuth, $cordovaCamera,$state) {
+
+    $ionicHistory.clearHistory();  //for clearing user login history
+
+    $scope.images = [];
+    $scope.fb = $firebaseAuth();
+    var fbAuth = $scope.fb.$getAuth();
+    var ref = firebase.database().ref();
+    var obj = $firebaseObject(ref);
+    
+    
+    if(fbAuth) {
+        var userReference = ref.child("users/" + fbAuth.uid);   //capture the user reference in data structure ,it navigates to specific user page in freebase
+        var syncArray = $firebaseArray(userReference.child("images"));  //binding specific node in firebase to an array object in angularjs
+        $scope.images = syncArray;
+    } else {
+        $state.go("firebase");  //directs to firebase page
+    }
+
+    
+    
+   //Called when button is clicked
+	$scope.showActionsheet = function() {
+	
+
+        
+        
+$ionicActionSheet.show({
+  titleText: 'what do you want to do with your photo?',
+   
+	cancelText: 'Cancel',
+  destructiveText: 'Delete',
+	
+  buttons: [
+       { text: '<b>Share</b> This' },
+       { text: 'Move' }
+     ]
+        
+                       
+});
+    
+      
+      $ionicActionSheet.show({
+  titleText: 'chose the way to share:',
+   
+	cancelText: 'Cancel',
+  destructiveText: 'Delete',
+	
+  buttons: [
+       { text: '<b>Share</b> with facebook' },
+       { text: '<b>Share</b> with twitter' }
+     ]
+        
+                       
+});
+        
+
+    }
+    
+    
+    
+    
+    
+    
+    $scope.upload = function() {
+        var options = {
+            quality : 75,
+            destinationType : Camera.DestinationType.DATA_URL,
+            sourceType : Camera.PictureSourceType.CAMERA,
+            allowEdit : true,
+            encodingType: Camera.EncodingType.JPEG,
+            popoverOptions: CameraPopoverOptions,
+            targetWidth: 500,
+            targetHeight: 500,
+            saveToPhotoAlbum: false
+        };
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+            syncArray.$add({image: imageData}).then(function() {
+                alert("Image has been uploaded");
+            });
+        }, function(error) {
+            console.error(error);
+        });
+    }
+
+});

993c474170ff9b13ad1b0f82e1be6655052b6f76
liuyunl777 <yunlongliu@yunlongs-mbp.attlocal.net>
2016-11-07 23:41:41 -0600

    lab 9 update

--- Lab 9/source/app.js
+++ Lab 9/source/app.js
@@ -1,25 +1,33 @@
-var http = require('http');
-
-// Options to be used by request
-var options = {
-    host: 'localhost',
-    port: '8081',
-    path: '/Main.html'
-};
-
-// Callback function is used to deal with response
-var callback = function(response){
-    // Continuously update stream with data
-    var body = '';
-    response.on('data', function(data) {
-        body += data;
-    });
-
-    response.on('end', function() {
-        // Data received completely.
-        console.log(body);
-    });
-}
-// Make a request to the server
-var req = http.request(options, callback);
-req.end();
+$(document).ready(
+    function() {
+        $.getJSON(
+            "http://127.0.0.1:8081/fish",
+            function(response) {
+                var intro = response["WikiInfo"][0]["intro"];
+                var imageUri = response["FlickrImageUri"][0]["uri"];
+                
+                $("#result").replaceWith(
+                    "<div id = 'result'>" +
+                  "<p style = \"color:red;font-size:1.5em;padding-left:1em;padding-top:2em;padding-bottom:1em;\"><u>flickr part</u></p><br>"+
+                    "<img src='" +
+                    imageUri +
+                    "' style = \"padding-left:1em;padding-top:1em;\">" +
+                  //  "<p style = \"font-size:1.5em;padding-left:1em;padding-top:2em;padding-bottom:1em;\"><b><u>fish</u></b></p>" +
+                 //   "<p align='justify' style = \"padding-left:1em;padding-right:1em;\">" +
+                 //   intro +
+                 //   "</p>" +
+                    "</div>"
+                );
+                $("#result1").replaceWith(
+                    "<div id = 'result1'>" +
+                   
+                    "<p style = \"color:red;font-size:1.5em;padding-left:1em;padding-top:2em;padding-bottom:1em;\"><b><u>wiki part</u></b><br><b><u>goldfish</u></b></p>" +
+                    "<p align='justify' style = \"padding-left:1em;padding-right:1em;\">" +
+                    intro +
+                    "</p>" +
+                    "</div>"
+                );
+            }
+        );
+    }
+);
--- Lab 9/source/server.js
+++ Lab 9/source/server.js
@@ -1,34 +1,65 @@
-var http = require('http');
-var fs = require("fs");
-var url = require('url');
-// Create a server
-http.createServer( function (request, response) {
-   // Parse the request containing file name
-   var pathname = url.parse(request.url).pathname;
+var express = require('express');
+var app = express();
+var request = require('request');
 
-   // Print the name of the file for which request is made.
-   console.log("Request for " + pathname + " received.");
+app.get(
+    '/fish',
+    function(req, res) {
+        var result = {
+            'WikiInfo': [],
+            'FlickrImageUri': []
+        };
+        
+        request(
+            "https://en.wikipedia.org/w/api.php?action=query&titles=" + "fish" + "&redirects=true&prop=extracts&exintro=true&explaintext=true&format=json&callback=?",
+            function(error, response, body) {
+                if (error) {
+                    return console.log('Error:', error);
+                }
+                if (response.statusCode !== 200) {
+                    return console.log('Invalid Status Code Returned:', response.statusCode);
+                }
+                body = body.substring(5);
+                body = body.substring(0, body.length - 1);
+                body = JSON.parse(body);
+                var ven = body.query.pages;
+                for (key in ven) {
+                    result.WikiInfo.push(
+                        {
+                            'intro': ven[key]["extract"]
+                        }
+                    );
+                }
+            }
+        );
+        
+        request(
+            "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=2ec2303f2f851adc69d5d238f3b76e41&per_page=1&tags=goldfish&accuracy=1&sort=relevance&extras=url_l&format=json&nojsoncallback=1",
+            function(error2, response2, body2) {
+                if (error2) {
+                    return console.log('Error:', error2);
+                }
+                if (response2.statusCode !== 200) {
+                    return console.log('Invalid Status Code Returned:', response2.statusCode);
+                }
+                body2 = JSON.parse(body2);
+                var imageUri = body2.photos.photo[0].url_l;
+                result.FlickrImageUri.push(
+                    {
+                        'uri': imageUri
+                    }
+                );
+                res.contentType('application/json');
+                res.write(JSON.stringify(result));
+                res.end();
+            }
+        );
+    }
+);
 
-   // Read the requested file content from file system
-   fs.readFile(pathname.substr(1), function (err, data) {
-      if (err) {
-         console.log(err);
-         // HTTP Status: 404 : NOT FOUND
-         // Content Type: text/plain
-         response.writeHead(404, {'Content-Type': 'text/html'});
-      }else {
-         //Page found
-         // HTTP Status: 200 : OK
-         // Content Type: text/plain
-         response.writeHead(200, {'Content-Type': 'text/html'});
+var server = app.listen(8081, function () {
+    var host = server.address().address;
+    var port = server.address().port;
 
-         // Write the content of the file to response body
-         response.write(data.toString());
-      }
-      // Send the response body
-      response.end();
-   });
-}).listen(8081);
-
-// Console will print the message
-console.log('Client Server running at http://127.0.0.1:8081/');
+    console.log("Example app listening at http://%s:%s", host, port);
+});
